014d9ea291e582337b3dc6f62b88da335fa7756c
conner <cdcharlebois@gmail.com>
2015-06-01 15:56:52 -0400

    comments

--- client/images.js
+++ client/images.js
@@ -16,7 +16,7 @@ Template.images.helpers({
 //
 Template.images.events({
 	'click canvas': function(e,t) {
-		//alert("you clicked on (" + e.offsetX +", " + e.offsetY + ")");
+		var TO_RADIANS = Math.PI/180;
 		var c = e.target;
 		var ctx = c.getContext('2d');
 		var img = document.getElementById('test'); // 235x235
@@ -24,14 +24,23 @@ Template.images.events({
 		(Session.get('scale') ? '' : Session.set('scale', 1));
 		var iw = 235 * Session.get('scale');
 		var ih = 235 * Session.get('scale');
-		// Session.set('angle')
 		//
-		// rotate context 
+		// move the center of the image to the center of the context
 		//
-		var TO_RADIANS = Math.PI/180;
-		ctx.translate(e.offsetX, e.offsetY); // center of the image on the canvas.
+		ctx.translate(e.offsetX, e.offsetY);
+		//
+		// rotate the context to the specified angle
+		//
 		ctx.rotate(Session.get('angle')*TO_RADIANS); 
+		//
+		// draw image
+		//  * note because we moved the context, the top-left part of the
+		//    image needs to slide up and left
+		//
 		ctx.drawImage(img, -iw/2, -ih/2, iw, ih); // (img, -width/2, -height/2)
+		//
+		// now put it back the way you found it
+		//
 		ctx.rotate(Session.get('angle')*TO_RADIANS*-1); // pi
 		ctx.translate(-1*e.offsetX, -1*e.offsetY)
 
