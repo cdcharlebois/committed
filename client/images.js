079bfa2ec425b707bdaeaf4845ebb1ff91a16f75
conner <cdcharlebois@gmail.com>
2015-06-01 15:42:07 -0400

    rotating Conner

--- client/images.js
+++ client/images.js
@@ -16,10 +16,24 @@ Template.images.helpers({
 //
 Template.images.events({
 	'click canvas': function(e,t) {
-		alert("you clicked on (" + e.offsetX +", " + e.offsetY + ")");
+		//alert("you clicked on (" + e.offsetX +", " + e.offsetY + ")");
 		var c = e.target;
 		var ctx = c.getContext('2d');
-		var img = document.getElementById('test');
-		ctx.drawImage(img, e.offsetX, e.offsetY);
+		var img = document.getElementById('test'); // 235x235
+		var iw = 235;
+		var ih = 235;
+		(Session.get('angle') ? '' : Session.set('angle', 0));
+		// Session.set('angle')
+		//
+		// rotate context 
+		//
+		var TO_RADIANS = Math.PI/180;
+		ctx.translate(e.offsetX, e.offsetY); // center of the image on the canvas.
+		ctx.rotate(Session.get('angle')*TO_RADIANS); 
+		ctx.drawImage(img, -iw/2, -ih/2, iw, ih); // (img, -width/2, -height/2, width, height)
+		ctx.rotate(Session.get('angle')*TO_RADIANS*-1); // pi
+		ctx.translate(-1*e.offsetX, -1*e.offsetY)
+
+		Session.set('angle', (Session.get('angle') + 45)*1);
 	}
 });
