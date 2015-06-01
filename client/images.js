9c3e302f35e66374ea28b6c1027c2c2bce9bfff4
conner <cdcharlebois@gmail.com>
2015-06-01 11:43:27 -0400

    added /images route for testing

---
+++ client/images.js
@@ -0,0 +1,21 @@
+// ---------------------------------------------------- //
+//    How to match two images based on target points    //
+// ---------------------------------------------------- //
+// 1. Place target points on images
+// 2. Use target points A(x,y) B(x,y) A'(x,y) B'(x,y) to determine...
+//    a) Scale -- using distance from A-B to A'-B' as a measure
+//    b) Rotation -- using slope from A-B to A'-B' as a measure
+//    c) Translation -- using dx/dy from A to A' as a measure
+Template.images.helpers({
+	foobar: function(){
+		return "Foobar";
+	}
+});
+//
+// Holy shit this works for any element...
+//
+Template.images.events({
+	'click img': function(e,t) {
+		alert("you clicked on (" + e.offsetX +", " + e.offsetY + ")");
+	}
+});
