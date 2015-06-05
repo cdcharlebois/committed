967fb925a5034ebda58421b14ed909448d697420
Conner Charlebois <cdcharlebois@gmail.com>
2015-06-04 22:25:49 -0400

    added fixtures

--- server/fixtures.js
+++ server/fixtures.js
@@ -4,11 +4,19 @@ Meteor.startup(function(){
 	//
 	if (Clothing.find().count() === 0){
 		Clothing.insert({
-			name: "Glasses"
-			img: "/images/glasses.png"
+			name: "Glasses",
+			img: "/images/glasses.png",
 			mapPoints: [
-				{x: , y: },
-				{x: , y: },
+				{x: 150, y: 100},
+				{x: 450, y: 100}
+			]
+		});
+		Clothing.insert({
+			name: "Red Dress",
+			img: "/images/dress.png",
+			mapPoints: [
+				{x: 74, y: 130},
+				{x: 155, y: 130}
 			]
 		});
 	}
