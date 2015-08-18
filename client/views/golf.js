e9da18512886401eb5e21b0bbb08f88a13601ee9
Conner Charlebois <cdcharlebois@gmail.com>
2015-08-17 22:02:24 -0400

    added practice sessions

--- client/views/golf.js
+++ client/views/golf.js
@@ -1,15 +1,41 @@
 Template.golf.helpers({
-	canAdd: function(){
-		return Session.get('canAdd');
+	canAddRound: function(){
+		return Session.get('canAddRound');
+	},
+	canAddPractice: function(){
+		return Session.get('canAddPractice');
 	},
 	rounds: function(){
 		return Rounds.find();
+	},
+	practices: function(){
+		return Practices.find({},{sort: {when: -1}})
+	},
+	avgFour: function(){
+		var p = Practices.find().fetch(),
+		    a = 0;
+		for (var i = 0; i < p.length; i++){
+			console.log(p[i].fourPct);
+			a += p[i].fourPct * 1;
+		}
+		console.log("a --> " + a)
+		return ( a / p.length ).toFixed(2);
+	},
+	avgSeven: function(){
+		var p = Practices.find().fetch(),
+		    a = 0;
+		for (var i = 0; i < p.length; i++){
+			console.log(p[i].sevenPct);
+			a += p[i].sevenPct * 1;
+		}
+		console.log("a --> " + a)
+		return ( a / p.length ).toFixed(2);
 	}
 });
 
 Template.golf.events({
 	'click #addRound': function(e,t){
-		Session.set('canAdd', true);
+		Session.set('canAddRound', true);
 	},
 	'click #doneRound': function(e,t){
 		var coursePar  = t.find("#coursePar").value*1,
@@ -20,6 +46,25 @@ Template.golf.events({
 			score: coursePar + toParTotal
 		});
 
-		Session.set('canAdd', false);
+		Session.set('canAddRound', false);
+	},
+	'click #addPractice': function(e,t){
+		Session.set('canAddPractice', true);
+	},
+	'click #donePractice': function(e,t){
+		var fourPct  = ((t.find('#puttsFrom4').value*1/12) * 100).toFixed(1),
+		    sevenPct = ((t.find('#puttsFrom7').value*1/12) * 100).toFixed(1);
+
+
+		if (!(fourPct == 0 || sevenPct == 0)) {
+			Practices.insert({
+				fourPct:  fourPct,
+				sevenPct: sevenPct,
+				when:     new Date()
+			});	
+		}
+		
+
+		Session.set('canAddPractice', false);
 	}
 });
--- committed.js
+++ committed.js
@@ -1,3 +1,4 @@
 Clothing = new Mongo.Collection('clothing');
 Matches = new Mongo.Collection('matches');
 Rounds = new Mongo.Collection('rounds');
+Practices = new Mongo.Collection('practices');
