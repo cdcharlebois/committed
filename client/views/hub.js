6b395493fb7bd1365902658aabf6d216f23374cc
Conner Charlebois <cdcharlebois@gmail.com>
2015-08-30 16:04:19 -0400

    added client confirmation

--- client/views/hub.js
+++ client/views/hub.js
@@ -1,7 +1,21 @@
-Template.hub.helpers({});
+Template.hub.helpers({
+	'isFlashMessage':function(){
+		return Session.get('isFlashMessage');
+	},
+	'flashMessage':function(){
+		return Session.get('flashMessage');
+	},
+	'flashError':function(){
+		return Session.get('flashError');
+	}
+});
 
 Template.hub.events({
-	'click #test':function(e, t){
+	'click #submit':function(e, t){
+
+		Session.set('isFlashMessage', null);
+		Session.set('flashError', null);
+		Session.set('flashMessage', null);
 		
 		var data = {
 			firstname: t.find('#firstName').value.trim(),
@@ -10,9 +24,21 @@ Template.hub.events({
 		}
 		console.log(data);
 		Meteor.call('createOrUpdateContact', data, function(err, res){
-			var pData = EJSON.parse(res.content);
-			console.log(pData);
-			alert('response received --> ' + pData.vid);
+			if (!res) {
+				//set flash message
+				Session.set('isFlashMessage', true);
+				Session.set('flashError', true);
+				Session.set('flashMessage', 'Sorry, your signup failed. Please check your entries.');
+			}
+			else {
+				// var pData = EJSON.parse(res.content);
+				// console.log(pData);
+				// alert('response received --> ' + pData.vid);	
+				Session.set('isFlashMessage', true);
+				Session.set('flashError', false);
+				Session.set('flashMessage', 'Thanks, '+data.firstname+'. You\'ve been signed up!');
+			}
+			
 		});
 		// Meteor.call('createOrUpdateContact', {}, function(err, res){
 		// 	var pData = EJSON.parse(res.content);
