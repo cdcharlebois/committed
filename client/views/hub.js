569b676295791c72a533dccfe4a261ffb6526da1
Conner Charlebois <cdcharlebois@gmail.com>
2015-08-30 14:05:46 -0400

    adding input form, dynamic API call

--- client/views/hub.js
+++ client/views/hub.js
@@ -1,11 +1,23 @@
 Template.hub.helpers({});
 
 Template.hub.events({
-	'click #test':function(){
-		Meteor.call('createOrUpdateContact', {}, function(err, res){
+	'click #test':function(e, t){
+		
+		var data = {
+			firstname: t.find('#firstName').value.trim(),
+			lastname: t.find('#lastName').value.trim(),
+			email: t.find('#email').value,
+		}
+		console.log(data);
+		Meteor.call('createOrUpdateContact', data, function(err, res){
 			var pData = EJSON.parse(res.content);
 			console.log(pData);
 			alert('response received --> ' + pData.vid);
 		});
+		// Meteor.call('createOrUpdateContact', {}, function(err, res){
+		// 	var pData = EJSON.parse(res.content);
+		// 	console.log(pData);
+		// 	alert('response received --> ' + pData.vid);
+		// });
 	}
 });
